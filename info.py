711320a69b382f7a43ec62fc0ee1612c5789533f
mkmehtab <mkmehtabkhan0@gmail.com>
2025-04-28 23:40:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7733173048:AAH1_jZLbZqqMzAHZiqQ110JXPe9WoS
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1586261625').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/@Arya_2530") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002689335197'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+pWiNfusNfck1ZTA1')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Or0VDrXEy8AyYzA1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002637440354').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
