98a664224da4ccfc742c59615a244caf21c71faf
mkmehtab <mkmehtabkhan0@gmail.com>
2025-05-01 01:22:12 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,75 +1,137 @@
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
+import sys
 
-def search_movie(query):
-    results = []
+while True:
+    option = int(input("What do you want to watch ?(enter 1 or 2 or 3)\n1- Series\n2- Movie\n3- Download a whole season\n"))
+    if 3 >= option >= 1:
+        break
+    else:
+        print("please enter 1 or 2 or 3")
 
-    # KatMovieHD Search
-    kat_url = f"https://katmoviehd.wales/?s={query.replace(' ', '+')}"
-    try:
-        response = requests.get(kat_url, timeout=10)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            articles = soup.find_all('article')
-            for article in articles:
-                title_tag = article.find('h2', class_='title')
-                if title_tag:
-                    title = title_tag.get_text(strip=True)
-                    link = title_tag.find('a')['href']
-                    # Fetch details from the movie page
-                    movie_resp = requests.get(link, timeout=10)
-                    if movie_resp.status_code == 200:
-                        movie_soup = BeautifulSoup(movie_resp.text, 'html.parser')
-                        # Extract quality and size from the content
-                        content = movie_soup.get_text()
-                        quality = 'Unknown'
-                        size = 'Unknown'
-                        for line in content.splitlines():
-                            if 'Quality' in line:
-                                quality = line.split(':')[-1].strip()
-                            if 'Size' in line:
-                                size = line.split(':')[-1].strip()
-                        results.append({
-                            'title': title,
-                            'quality': quality,
-                            'size': size,
-                            'link': link
-                        })
-    except Exception as e:
-        print(f"Error fetching from KatMovieHD: {e}")
+if option == 1:                      # Watch a series
+    series_name = input("Enter series name: ")
+    series_name.rstrip(" ")
 
-    # HDHub4u Search
-    hdhub_url = f"https://hdhub4u.graphics/?s={query.replace(' ', '+')}"
-    try:
-        response = requests.get(hdhub_url, timeout=10)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            articles = soup.find_all('article')
-            for article in articles:
-                title_tag = article.find('h2', class_='title')
-                if title_tag:
-                    title = title_tag.get_text(strip=True)
-                    link = title_tag.find('a')['href']
-                    # Fetch details from the movie page
-                    movie_resp = requests.get(link, timeout=10)
-                    if movie_resp.status_code == 200:
-                        movie_soup = BeautifulSoup(movie_resp.text, 'html.parser')
-                        # Extract quality and size from the content
-                        content = movie_soup.get_text()
-                        quality = 'Unknown'
-                        size = 'Unknown'
-                        for line in content.splitlines():
-                            if 'Quality' in line:
-                                quality = line.split(':')[-1].strip()
-                            if 'Size' in line:
-                                size = line.split(':')[-1].strip()
-                        results.append({
-                            'title': title,
-                            'quality': quality,
-                            'size': size,
-                            'link': link
-                        })
-    except Exception as e:
-        print(f"Error fetching from HDHub4u: {e}")
+    # Preparing our html page and soup
+    html_page = requests.get("https://mycima.tube/search/"+series_name+"/list/series")
+    soup = BeautifulSoup(html_page.content, "lxml")
 
-    return results
+    # Scraping links and titles of series resulted from the query
+    recommended_series = soup.find_all("div", {"class": "Thumb--GridItem"})
+    links_of_recommended_series = []
+
+    print("Which series do you want to watch?")
+
+    counter_series = 1
+    for i in recommended_series:
+        link = i.find("a")
+        links_of_recommended_series.append(link.get("href"))
+        print(str(counter_series)+"- "+link.get("title"))
+        counter_series += 1
+
+    series_number = int(input("Enter the number corresponding to your wanted series: "))
+
+    # Preparing html page and soup for desired series
+    html_page = requests.get(links_of_recommended_series[series_number-1])
+    soup = BeautifulSoup(html_page.content, "lxml")
+
+
+    # Scraping links and titles of seasons of the desired series
+    temp_list = soup.find("div", {"class": "List--Seasons--Episodes"})
+    if temp_list is None:
+        print("There is only one season available")
+        temp_list = soup.find("div", {"class": "Seasons--Episodes"})
+        episodes_links = temp_list.findAll("a")
+    else:
+        seasons_links = temp_list.findAll("a")  # i.get("href")
+        season = int(input("There are " + str(len(seasons_links)) + " Seasons, Enter your desired season number: "))
+        # Preparing html page and soup to be used to scrape episodes links
+        html_page = requests.get(seasons_links[season - 1].get("href"))
+        soup = BeautifulSoup(html_page.content, "lxml")
+
+        # Scraping episodes links and titles
+        temp_list = soup.find("div", {"class": "Episodes--Seasons--Episodes"})
+        episodes_links = temp_list.findAll("a")
+        episodes_links.reverse()
+
+    print("There are " + str(len(episodes_links)) + " episodes")
+    ep1 = int(input("Enter episode number (from): "))
+    ep2 = int(input("Enter episode number (to): "))
+    counter_episodes = ep2
+    while counter_episodes >= ep1:      # Iterating through desired episodes
+        html_page = requests.get(episodes_links[counter_episodes-1].get("href"))
+        soup = BeautifulSoup(html_page.content, "lxml")
+        link = soup.find("iframe",{"name":"watch"})
+        watching_link = link.get("data-lazy-src")
+        result = requests.get(watching_link)
+        webbrowser.open(watching_link)
+        counter_episodes -= 1
+
+elif option == 2:       # Watch a movie
+    movie_name = input("Enter movie name: ")
+    movie_name.rstrip(" ")
+
+    html_page = requests.get("https://mycima.tube/search/"+movie_name)
+    soup = BeautifulSoup(html_page.content, "lxml")
+
+    recommended_movies = soup.find_all("div", {"class": "Thumb--GridItem"})
+    links_of_recommended_movies= []
+
+    print("Which Movie do you want to watch?")
+
+    counter_series = 1
+    for i in recommended_movies:
+        link = i.find("a")
+        links_of_recommended_movies.append(link.get("href"))
+        print(str(counter_series) + "- " + link.get("title"))
+        counter_series += 1
+
+    movie_number = int(input("Enter the number corresponding to your wanted movie: "))
+    movie_link = links_of_recommended_movies[movie_number-1]
+
+    html_page = requests.get(movie_link)
+    soup = BeautifulSoup(html_page.content, "lxml")
+
+    link = soup.find("iframe", {"name": "watch"})
+    watching_link = link.get("data-lazy-src")
+    webbrowser.open(watching_link)
+
+
+else:       # Download a whole season
+    series_name = input("Enter series name: ")
+    series_name.rstrip(" ")
+    series_name = series_name.replace(" ", "-")
+    season = int(input("Enter Season number: "))
+    html_page = requests.get("https://mycima.tube/series/%d9%85%d8%b3%d9%84%d8%b3%d9%84-"+str(series_name)+"-%d9%85%d9%88%d8%b3%d9%85-"+str(season)+"-")
+    if html_page.status_code == 404:
+        html_page = requests.get("https://mycima.tube/series/%D9%85%D9%88%D8%B3%D9%85-"+str(season)+"-%D9%85%D8%B3%D9%84%D8%B3%D9%84-"+str(series_name))
+    if html_page.status_code == 404:
+        html_page = requests.get("https://mycima.tube/series/%d9%85%d9%88%d8%b3%d9%85-"+str(season)+"-"+str(series_name))
+    if html_page.status_code == 404:
+        html_page = requests.get("https://mycima.tube/series/"+str(series_name)+"-%d9%85%d9%88%d8%b3%d9%85-"+str(season))
+
+    soup = BeautifulSoup(html_page.content, "lxml")
+    x = soup.find('ul', {'class': "Season--Download--Mycima--Single"})
+    if x is None:
+        input("This feature is not available for "+str(series_name)+" season "+str(season)+"! \nPress any key to exit")
+        sys.exit()
+
+
+    temp = x.findAll("a")
+    qualities = soup.find_all("resolution")
+    links = []
+    for i in temp:
+        links.append(i.get("href"))
+
+    counter_qualities = 1
+    for i in qualities:
+        print(str(counter_qualities)+"- " + i.text)
+        counter_qualities += 1
+
+    quality = int(input("Enter your desired quality: "))
+    webbrowser.open(links[quality-1])
+
+
+input("Enter any key to exit: ")
