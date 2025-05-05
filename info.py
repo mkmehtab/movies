0814200954d621e04098c31eacb4f04836ecd33e
mkmehtab <mkmehtabkhan0@gmail.com>
2025-05-06 00:30:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Or0VDrXEy8AyYz
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002637440354').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yashdd:yashdd@yashdd.a9gbuqp.mongodb.net/?=true&w=majority&appName=yashdd")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb://yashdd:yashdd@yashdd.a9gbuqp.mongodb.net/?=true&w=majority&appName=yashdd")
 DATABASE_NAME = environ.get('DATABASE_NAME', "yashdd")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
